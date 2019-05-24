2e2437ee757ab6819b864d9ad4ac0897e58eb11d
Arran Fraser <arran@madebyframe.com>
2019-05-24 10:28:33 -0300

    mobile menu improved

--- web/app/themes/swi/resources/views/partials/header.blade.php
+++ web/app/themes/swi/resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="banner">
   <div class="container">
     <div class="row">
-      <div class="col-12 col-sm-6 order-sm-12">
+      <div class="col-12 col-lg-4 col-xl-6 order-lg-12">
         @if( $custom_logo = get_theme_mod('custom_logo') )
           <a class="" href="{{ home_url('/') }}"><img src="{!! esc_url( wp_get_attachment_image_src( $custom_logo )[0] ) !!}" class="w-100 h-auto" /></a>
         @else
@@ -10,7 +10,7 @@
 
       </div>
 
-      <div class="col-12 col-sm-6 order-sm-1 d-flex">
+      <div class="col-12 col-lg-8 col-xl-6 order-lg-1  d-flex">
 
         <nav class="nav-primary align-self-center">
           @if (has_nav_menu('primary_navigation'))
